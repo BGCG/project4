50934ee64a683cebff4e76a95131c22cbcb32922
BGCG <billie.griffith4@gmail.com>
2023-04-24 07:30:39 +0000

    Create view for recipe list on homepage

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Recipe
 
-# Create your views here.
+
+class RecipeList(generic.ListView):
+
+    model = Recipe
+    queryset = Recipe.objects.filter(article_approved=True, status=1).order_by('-publication_date')
+    template_name = 'index.html'
+    paginate_by = 4
+
+
