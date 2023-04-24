f1507fa92f40d69a343fff1eb72c5decf1b0835b
BGCG <billie.griffith4@gmail.com>
2023-04-24 09:52:45 +0000

    Add required settings for allauth

--- veganrecipes/settings.py
+++ veganrecipes/settings.py
@@ -51,6 +51,11 @@ INSTALLED_APPS = [
     'blog',
 ]
 
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -115,6 +120,9 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+# Email verification
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 
 # Internationalization
