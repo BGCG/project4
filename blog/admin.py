78de2750765b5210f512daaf13639e2b61a72ac8
BGCG <billie.griffith4@gmail.com>
2023-04-28 10:23:29 +0000

    Remove trailing whitespace from admin file

--- blog/admin.py
+++ blog/admin.py
@@ -14,7 +14,7 @@ class RecipeAdmin(SummernoteModelAdmin):
     search_fields = ['title', 'instructions', 'ingredients']
 
     actions = ['approve_recipe']
-  
+
     def approve_recipe(self, request, queryset):
         queryset.update(article_approved=True)
 
@@ -29,4 +29,3 @@ class CommentAdmin(admin.ModelAdmin):
 
     def approve_comments(self, request, queryset):
         queryset.update(approved=True)
-
