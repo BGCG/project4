ed30780805c8dd38ad9a2048550d719007178813
BGCG <billie.griffith4@gmail.com>
2023-04-28 10:26:42 +0000

    Remove commented out code from form.py file

--- blog/forms.py
+++ blog/forms.py
@@ -1,18 +1,14 @@
 from .models import Recipe, Comment
 from django import forms
-#from django_summernote.widgets import SummernoteWidget
 
 
 class PostForm(forms.ModelForm):
 
     class Meta:
         model = Recipe
-        fields = ('title', 'recipe_image', 'ingredients', 'instructions', 'taste_type', 'skill_level', 'preparation_time', 'status')
+        fields = ('title', 'recipe_image', 'ingredients', 'instructions',
+                  'taste_type', 'skill_level', 'preparation_time', 'status')
         labels = {'preparation_time': 'Preparation Time (minutes)'}
-        # widgets = {
-        #     'ingredients': SummernoteWidget(),
-        #     'instructions': SummernoteWidget(),
-        # }
 
 
 class CommentForm(forms.ModelForm):
