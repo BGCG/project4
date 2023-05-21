ee788d8da2157c057210b632075f688b9afbebb0
BGCG <billie.griffith4@gmail.com>
2023-05-21 13:17:59 +0000

    Remove cloudinary config var for transformation as not functional

--- veganrecipes/settings.py
+++ veganrecipes/settings.py
@@ -164,13 +164,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
-CLOUDINARY = {
-    'transformation': [
-        {'format': 'webp'}
-    ]
-}
-
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
