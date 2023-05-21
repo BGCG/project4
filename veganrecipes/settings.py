dba2603da3c4ad58d124de6752e4b999891012fd
BGCG <billie.griffith4@gmail.com>
2023-05-21 16:57:35 +0000

    Get settings ready for deployment and AmIResponsive screenshotting

--- veganrecipes/settings.py
+++ veganrecipes/settings.py
@@ -28,9 +28,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-X_FRAME_OPTIONS = 'SAMEORIGIN'
+#X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['vegan-recipe-app.herokuapp.com', 'localhost', '8000-bgcg-project4-pykluy3ro1k.ws-eu97.gitpod.io']
 
